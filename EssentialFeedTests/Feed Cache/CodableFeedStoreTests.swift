e6870d5671151b3a04f05cfc4d029e6cfeec3635
sanjay <sanjaykumarmurthy@gmail.com>
2022-05-20 05:43:20 +0530

    Make `EssentialFeedAPIEndToEndTests` target support macOS and iOS since its platform independent

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -142,7 +142,7 @@ class CodableFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
     }
     
     private func cachesDirectory() -> URL {
-        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+        return FileManager.default.urls(for: .cachesDirectory, in: .systemDomainMask).first!
     }
     
     private func setupEmptyStoreState() {

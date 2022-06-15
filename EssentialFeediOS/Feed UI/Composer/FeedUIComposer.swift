4297df4cbd8f4e36019015a42f2903fa23492702
sanjay <sanjaykumarmurthy@gmail.com>
2022-06-15 23:14:16 +0530

    * Move MVVM ViewModels to a new Feed Presentation group to clarify the architectural separation between Presentation and framework-specific UI * Add FeedPresenter holding feed loading presentation logic (loading state and loaded feed) * Move memory management to Composer layer. The FeedPresenter shouldn't have to know or handle their dependencies lifetime. * Remove unused FeedViewModel (it was replaced by the new `FeedPresenter`).

--- EssentialFeediOS/Feed UI/Composer/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composer/FeedUIComposer.swift
@@ -12,11 +12,14 @@ public final class FeedUIComposer {
     private init() {}
     
     public static func feedComposeWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
-        let feedViewModel        = FeedViewModel(feedLoader: feedLoader)
-        let refreshController    = FeedRefreshViewController(viewModel: feedViewModel)
+        let feedPresenter        = FeedPresenter(feedLoader: feedLoader)
+        let refreshController    = FeedRefreshViewController(presenter: feedPresenter)
         let feedController       = FeedViewController(refreshController: refreshController)
-        feedViewModel.onFeedLoad = adaptFeedToCellControllers(forwardingTo: feedController, loader: imageLoader)
-        return feedController
+        
+        feedPresenter.loadingView = WeakRefVirtualProxy(refreshController)
+        feedPresenter.feedView = FeedViewAdapter(controller: feedController, imageLoader: imageLoader)
+        
+        return feedController 
     }
     
     private static func adaptFeedToCellControllers(forwardingTo controller: FeedViewController, loader: FeedImageDataLoader) -> ([FeedImage]) -> Void {
@@ -24,6 +27,36 @@ public final class FeedUIComposer {
             controller?.tableModel = feed.map { model in
                 FeedImageCellController(viewModel: FeedImageViewModel(model: model, imageLoader: loader, imageTransformer: UIImage.init))
             }
+        }
+    }
+}
+
+private final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+    
+    init(_ object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView{
+    func display(isLoading: Bool) {
+        object?.display(isLoading: isLoading)
+    }
+}
+
+private final class FeedViewAdapter: FeedView {
+    private weak var controller: FeedViewController?
+    private let imageLoader: FeedImageDataLoader
+    
+    init(controller: FeedViewController, imageLoader: FeedImageDataLoader) {
+        self.controller = controller
+        self.imageLoader = imageLoader
+    }
+    
+    func display(feed: [FeedImage]) {
+        controller?.tableModel = feed.map { model in
+            FeedImageCellController(viewModel: FeedImageViewModel(model: model, imageLoader: imageLoader, imageTransformer: UIImage.init))
         }
     }
 }

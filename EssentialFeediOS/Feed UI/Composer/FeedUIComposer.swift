66abb1eb5c075b010a11144bfc27860cff6d0d1d
sanjay <sanjaykumarmurthy@gmail.com>
2022-06-16 21:53:10 +0530

    Decouple FeedPresenter from FeedLoader with an adapter in the Composition layer

--- EssentialFeediOS/Feed UI/Composer/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composer/FeedUIComposer.swift
@@ -12,12 +12,13 @@ public final class FeedUIComposer {
     private init() {}
     
     public static func feedComposeWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
-        let feedPresenter        = FeedPresenter(feedLoader: feedLoader)
-        let refreshController    = FeedRefreshViewController(loadFeed: feedPresenter.loadFeed)
-        let feedController       = FeedViewController(refreshController: refreshController)
+        let presenter         = FeedPresenter()
+        let presenterAdapter  = FeedLoaderPresentationAdapter(feedLoader: feedLoader, presenter: presenter)
+        let refreshController = FeedRefreshViewController(loadFeed: presenterAdapter.loadFeed)
+        let feedController    = FeedViewController(refreshController: refreshController)
         
-        feedPresenter.loadingView = WeakRefVirtualProxy(refreshController)
-        feedPresenter.feedView = FeedViewAdapter(controller: feedController, imageLoader: imageLoader)
+        presenter.loadingView = WeakRefVirtualProxy(refreshController)
+        presenter.feedView = FeedViewAdapter(controller: feedController, imageLoader: imageLoader)
         
         return feedController 
     }
@@ -56,7 +57,30 @@ private final class FeedViewAdapter: FeedView {
     
     func display(_ viewModel: FeedViewModel) {
         controller?.tableModel = viewModel.feed.map { model in
-            FeedImageCellController(viewModel: FeedImageViewModel(model: model, imageLoader: imageLoader, imageTransformer: UIImage.init))
+            FeedImageCellController(viewModel:
+                                        FeedImageViewModel(model: model, imageLoader: imageLoader, imageTransformer: UIImage.init))
+        }
+    }
+}
+
+private final class FeedLoaderPresentationAdapter {
+    private let feedLoader: FeedLoader
+    private let presenter: FeedPresenter
+    
+    init(feedLoader: FeedLoader, presenter: FeedPresenter) {
+        self.feedLoader = feedLoader
+        self.presenter = presenter
+    }
+    
+    func loadFeed() {
+        presenter.didStartLoadingFeed()
+        feedLoader.load { [weak self] result in
+            switch result {
+            case let .success(feed):
+                self?.presenter.didFinishLoadingFeed(with: feed)
+            case let .failure(error):
+                self?.presenter.didFinishWithError(with: error)
+            }
         }
     }
 }

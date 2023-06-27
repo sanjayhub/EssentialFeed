17347868223b97186c3ae8c8d3a34d9a0426ac89
sanjay <sanjaykumarmurthy@gmail.com>
2023-06-27 06:52:04 +0530

    CoreDataFeedStore.retrieve() delivers found values on non-empty cache

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -7,16 +7,18 @@
 
 import CoreData
 
+@objc(ManagedCache)
 private class ManagedCache: NSManagedObject {
     @NSManaged var timestamp: Date
     @NSManaged var feed: NSOrderedSet
 }
 
+@objc(ManagedFeedImage)
 private class ManagedFeedImage: NSManagedObject {
     @NSManaged var id: UUID
     @NSManaged var imageDescription: String?
     @NSManaged var location: String?
-    @NSManaged var url: String?
+    @NSManaged var url: URL
     @NSManaged var cache: ManagedCache
 }
 
@@ -34,11 +36,49 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        
+        let context = self.context
+        context.perform {
+            do {
+                let managedCache = ManagedCache(context: context)
+                managedCache.timestamp = timestamp
+                managedCache.feed = NSOrderedSet(array: feed.map{ local in
+                    let managed = ManagedFeedImage(context: context)
+                    managed.id = local.id
+                    managed.imageDescription = local.description
+                    managed.url = local.url
+                    managed.location = local.location
+                    return managed
+                })
+                try context.save()
+                completion(.success(()))
+            } catch {
+                completion(.failure(error))
+            }
+        }
     }
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        completion(.success(.none))
+        let context = self.context
+        context.perform {
+            do {
+                let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
+                request.returnsObjectsAsFaults = false
+                if let cache = try context.fetch(request).first {
+                    completion(.success(
+                        CachedFeed(
+                            feed: cache.feed
+                                   .compactMap({ ($0 as? ManagedFeedImage) })
+                                   .map {
+                                      LocalFeedImage(id: $0.id, description: $0.imageDescription, location: $0.location, url: $0.url)
+                            },
+                            timestamp: cache.timestamp)))
+                } else {
+                    completion(.success(.none))
+                }
+            } catch {
+                completion(.failure(error))
+            }
+        }
     }
 }
 

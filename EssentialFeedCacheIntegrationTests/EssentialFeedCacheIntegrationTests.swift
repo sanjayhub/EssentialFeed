3dffed741900cd0cced686bc23682dc6a3e6d42a
sanjay <sanjaykumarmurthy@gmail.com>
2023-06-28 19:27:31 +0530

    Include `EssentialFeedCacheIntegrationTests` test target in the CI scheme to guarantee we build and run all cache integration tests as part of the continuous integration pipeline.

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Kumar, Sanjay (623) on 28/06/23.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+}

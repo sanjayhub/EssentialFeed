23fb1fc01365db71a0f27c338350eb7c60e6c010
sanjay <sanjaykumarmurthy@gmail.com>
2023-07-01 16:27:47 +0530

    Extract cache feed image data use case into the new `CacheFeedImageDataUseCaseTests` test case class

--- EssentialFeedTests/Feed Cache/LoadFeedImageDataFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedImageDataFromCacheUseCaseTests.swift
@@ -73,17 +73,7 @@ class LoadFeedImageDataFromCacheUseCaseTests: XCTestCase {
         store.completeRetrieval(with: anyData())
         XCTAssertTrue(receivedMsg.isEmpty)
     }
-    
-    func test_saveImageDataForURL_requestsImageDataInsertionForURL() {
-        let (sut, store) = makeSUT()
-        let url = anyURL()
-        let data = anyData()
-        sut.save(data, for: url) { _ in
-            
-        }
-        XCTAssertEqual(store.receivedMessages, [.insert(data: data, for: url)])
-    }
-    
+        
     //MARK: - Helpers
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: FeedImageDataStoreSpy) {
         let store = FeedImageDataStoreSpy()
